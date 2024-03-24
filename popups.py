8d407facc2bd35108143ed37c530e86c4482b182
Khadorstorm <2596055455@qq.com>
2024-03-24 22:33:53 +0100

    pushed my playground anyway for I don't get to fork from myself and kanske ni vill kolla

--- popups.py
+++ popups.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QDialog, QLineEdit, QPushButton, QVBoxLayout, QLabel, QMainWindow, QWidget, QHBoxLayout
-from tuben.qt_test import Ui_TubeN
+from qt_test import Ui_TubeN
 
 #接收两个输入
 class InputDialog(QDialog):
@@ -14,14 +14,14 @@ class InputDialog(QDialog):
         mainLayout.addWidget(QLabel("Enter the tube parameters,\nmax 4 sections:Separate by comma(,)"))
         # Create first input row
         row1Layout = QHBoxLayout()
-        row1Layout.addWidget(QLabel("Input 1:"))
+        row1Layout.addWidget(QLabel("Length(s):"))
         self.input1 = QLineEdit(self)
         row1Layout.addWidget(self.input1)
         mainLayout.addLayout(row1Layout)
 
         # Create second input row
         row2Layout = QHBoxLayout()
-        row2Layout.addWidget(QLabel("Input 2:"))
+        row2Layout.addWidget(QLabel("Area(s):"))
         self.input2 = QLineEdit(self)
         row2Layout.addWidget(self.input2)
         mainLayout.addLayout(row2Layout)
@@ -43,18 +43,22 @@ class MainWindow(QMainWindow, Ui_TubeN):
         self.setupUi(self)
         self.pushButton_add.clicked.connect(self.openInputDialog)
         #self.button.clicked.connect(self.openInputDialog)
-
+        self.L = []
+        self.A = []
         #self.setCentralWidget(self.button)
 
     def openInputDialog(self):
         dialog = InputDialog(self)
         dialog.setWindowTitle("add")
-
-
         if dialog.exec_():
-            input1, input2 = dialog.getInputs()
-            print("Input 1:", input1)
-            print("Input 2:", input2)
+            lengths, areas = dialog.getInputs()
+            print(type(lengths))
+            print("Length(s):", lengths)
+            print("Area(s):", areas)
+            le = [float(l) for l in lengths.split(',')]
+            ar = [float(a) for a in areas.split(',')]
+            self.L.append(le)
+            self.A.append(ar)
             # Process the inputs or pass them to another part of the program here
 
 def main():
